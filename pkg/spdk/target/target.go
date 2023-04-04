3430d34c53c8d055ac96def87c569fc0cb1acde1
Shuo Wu <shuo.wu@suse.com>
2023-04-04 21:51:28 +0800

    Add SPDK target launch API
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

---
+++ pkg/spdk/target/target.go
@@ -0,0 +1,59 @@
+package target
+
+import (
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/longhorn/go-spdk-helper/pkg/spdk/client"
+)
+
+const (
+	SPDKScriptsDir  = "scripts"
+	SPDKSetupScript = "setup.sh"
+	SPDKTGTBinary   = "build/bin/spdk_tgt"
+)
+
+func Setup(spdkDir string, execute func(name string, args []string) (string, error)) (err error) {
+	setupScriptPath := filepath.Join(spdkDir, SPDKScriptsDir, SPDKSetupScript)
+	setupOpts := []string{
+		"-c",
+		setupScriptPath,
+	}
+
+	resetOpts := []string{
+		"-c",
+		setupScriptPath,
+		"reset",
+	}
+
+	if _, err := execute("sh", resetOpts); err != nil {
+		return err
+	}
+	if _, err := execute("sh", setupOpts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func StartTarget(spdkDir string, execute func(name string, args []string) (string, error)) (err error) {
+	if spdkCli, err := client.NewClient(); err == nil {
+		if _, err := spdkCli.BdevGetBdevs("", 0); err == nil {
+			logrus.Info("Detected running spdk_tgt, skipped the target starting")
+			return nil
+		}
+	}
+
+	if err := Setup(spdkDir, execute); err != nil {
+		return err
+	}
+
+	tgtOpts := []string{
+		"-c",
+		filepath.Join(spdkDir, SPDKTGTBinary),
+	}
+
+	_, err = execute("sh", tgtOpts)
+	return err
+}
