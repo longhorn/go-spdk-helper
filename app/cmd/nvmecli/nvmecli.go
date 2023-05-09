d8c63aafc5ce0fe9145edfae8d04ee2161de8b8f
Shuo Wu <shuo.wu@suse.com>
2023-05-09 21:59:52 +0800

    app: Add cmd for NVMe initiator
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- app/cmd/nvmecli/nvmecli.go
+++ app/cmd/nvmecli/nvmecli.go
@@ -19,6 +19,8 @@ func Cmd() cli.Command {
 			ConnectCmd(),
 			DisconnectCmd(),
 			GetCmd(),
+			StartCmd(),
+			StopCmd(),
 		},
 	}
 }
@@ -156,6 +158,106 @@ func get(c *cli.Context) error {
 		return err
 	}
 	fmt.Println(string(getRespJSON))
+
+	return nil
+}
+
+func StartCmd() cli.Command {
+	return cli.Command{
+		Name: "start",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "name",
+				Usage: "The name of initiator. The initiator will make the device to `/dev/longhorn/<name>`.",
+			},
+			cli.StringFlag{
+				Name:  "traddr",
+				Usage: "NVMe-oF target address: a ip or BDF.",
+				Value: types.LocalIP,
+			},
+			cli.StringFlag{
+				Name:  "trsvcid",
+				Usage: "NVMe-oF target trsvcid: a port number.",
+			},
+			cli.StringFlag{
+				Name:  "nqn",
+				Usage: "NVMe-oF target subsystem nqn.",
+			},
+			cli.StringFlag{
+				Name:  "host-proc",
+				Usage: "The host proc path of namespace executor. Empty means not using a namespace executor. By default empty.",
+				Value: "",
+			},
+		},
+		Usage: "Start a NVMe-oF initiator and make a device based on the name: start --name <NAME> --traddr <IP> --trsvcid <PORT NUMBER> --nqn <SUBSYSTEM NQN>",
+		Action: func(c *cli.Context) {
+			if err := start(c); err != nil {
+				logrus.WithError(err).Fatalf("Error running initiator start command")
+			}
+		},
+	}
+}
+
+func start(c *cli.Context) error {
+	initiator, err := nvme.NewInitiator(c.String("name"), c.String("nqn"), c.String("host-proc"))
+	if err != nil {
+		return err
+	}
+
+	if err := initiator.Start(c.String("traddr"), c.String("trsvcid")); err != nil {
+		return err
+	}
+
+	startRespJSON, err := json.MarshalIndent(
+		map[string]string{
+			"controller_name": initiator.GetControllerName(),
+			"namespace_name":  initiator.GetNamespaceName(),
+			"endpoint":        initiator.GetEndpoint(),
+		}, "", "\t")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(startRespJSON))
+
+	return nil
+}
+
+func StopCmd() cli.Command {
+	return cli.Command{
+		Name: "stop",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "name",
+				Usage: "The name of initiator. The initiator will make the device to `/dev/longhorn/<name>`.",
+			},
+			cli.StringFlag{
+				Name:  "nqn",
+				Usage: "NVMe-oF target subsystem nqn.",
+			},
+			cli.StringFlag{
+				Name:  "host-proc",
+				Usage: "The host proc path of namespace executor. Empty means not using a namespace executor. By default empty.",
+				Value: "",
+			},
+		},
+		Usage: "Stop a NVMe-oF initiator and remove the corresponding device: stop --name <NAME> --nqn <SUBSYSTEM NQN>",
+		Action: func(c *cli.Context) {
+			if err := stop(c); err != nil {
+				logrus.WithError(err).Fatalf("Error running initiator stop command")
+			}
+		},
+	}
+}
+
+func stop(c *cli.Context) error {
+	initiator, err := nvme.NewInitiator(c.String("name"), c.String("nqn"), c.String("host-proc"))
+	if err != nil {
+		return err
+	}
+
+	if err := initiator.Stop(); err != nil {
+		return err
+	}
 
 	return nil
 }
