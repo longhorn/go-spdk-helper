21840f47468759f969f5c5ca1ff702c8539b9690
Derek Su <derek.su@suse.com>
2023-06-08 09:33:02 +0800

    Refine log messages
    Longhorn 5744
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/longhorn/go-spdk-helper/app/cmd/spdktgt"
 	"os"
+
+	"github.com/longhorn/go-spdk-helper/app/cmd/spdktgt"
 
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
@@ -43,6 +44,6 @@ func main() {
 		spdktgt.Cmd(),
 	}
 	if err := a.Run(os.Args); err != nil {
-		logrus.WithError(err).Fatal("Error when executing command")
+		logrus.WithError(err).Fatal("Failed to execute command")
 	}
 }
