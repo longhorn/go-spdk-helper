579110a4706cec040d05c8ff6f9433641f95c663
Derek Su <derek.su@suse.com>
2023-09-27 14:34:23 +0800

    Use nvme cli in container instead
    Longhorn 6798
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- pkg/nvme/initiator.go
+++ pkg/nvme/initiator.go
@@ -49,8 +49,7 @@ func NewInitiator(name, subsystemNQN, hostProc string) (*Initiator, error) {
 	}
 
 	// If transportAddress or transportServiceID is empty, the initiator is still valid for stopping
-
-	executor, err := util.GetExecutorByHostProc(hostProc)
+	executor, err := util.GetExecutorByHostProc("")
 	if err != nil {
 		return nil, err
 	}
