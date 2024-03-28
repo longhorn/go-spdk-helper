7ab2393959d95c50621d53e61acd0bf09a33b24c
Derek Su <derek.su@suse.com>
2024-03-28 05:25:32 +0000

    GetDiskStatus: return *types.DiskStatus rather than a json string
    Longhorn 7672
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- pkg/spdk/setup/setup.go
+++ pkg/spdk/setup/setup.go
@@ -1,10 +1,12 @@
 package setup
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
 	commonNs "github.com/longhorn/go-common-libs/ns"
+	"github.com/pkg/errors"
 
 	"github.com/longhorn/go-spdk-helper/pkg/types"
 )
@@ -74,9 +76,9 @@ func GetDiskDriver(deviceAddr string, executor *commonNs.Executor) (string, erro
 	return extractJSON(outputStr)
 }
 
-func GetDiskStatus(deviceAddr string, executor *commonNs.Executor) (string, error) {
+func GetDiskStatus(deviceAddr string, executor *commonNs.Executor) (*types.DiskStatus, error) {
 	if deviceAddr == "" {
-		return "", fmt.Errorf("device address is empty")
+		return nil, fmt.Errorf("device address is empty")
 	}
 
 	cmdArgs := []string{
@@ -87,10 +89,21 @@ func GetDiskStatus(deviceAddr string, executor *commonNs.Executor) (string, erro
 
 	outputStr, err := executor.Execute(nil, "bash", cmdArgs, types.ExecuteTimeout)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return extractJSON(outputStr)
+	jsonsStr, err := extractJSON(outputStr)
+	if err != nil {
+		return nil, err
+	}
+
+	var diskStatus types.DiskStatus
+	err = json.Unmarshal([]byte(jsonsStr), &diskStatus)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to unmarshal disk status for %s", deviceAddr)
+	}
+
+	return &diskStatus, nil
 }
 
 func extractJSON(outputStr string) (string, error) {
