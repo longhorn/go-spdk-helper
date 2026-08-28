172f07ff4e98098d71ba1622da960ee2333ea555
Derek Su <derek.su@suse.com>
2026-08-28 01:10:21 +0000

    fix(porting): drop dead DmaDeviceID from BdevInfoBasic memory_domains
    Longhorn 13834
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- pkg/spdk/types/bdev.go
+++ pkg/spdk/types/bdev.go
@@ -76,7 +76,6 @@ type BdevInfoBasic struct {
 	SupportedIoTypes SupportedIoTypes `json:"supported_io_types"`
 
 	MemoryDomains []struct {
-		DmaDeviceID   string `json:"dma_device_id"`
 		DmaDeviceType string `json:"dma_device_type"`
 	} `json:"memory_domains,omitempty"`
 }
